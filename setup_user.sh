ad485a5cb68e5c7608303ea4216ef37664e4d654
00k <likangd@gmail.com>
2017-04-13 11:16:57 +0800

    update setup_user

--- setup_user.sh
+++ setup_user.sh
@@ -11,12 +11,17 @@ source host.sh
 
 echo "add user ..."
 for ((i=0; i<HOST_NUM; i++)); do
+    ssh -o StrictHostKeyChecking=no ${HOST[$i]} "rm -fr /home/$username/.ssh/*"
     ssh -o StrictHostKeyChecking=no ${HOST[$i]} "/usr/sbin/useradd $username; sudo -u $username ssh-keygen -t rsa  -N '' -f /home/$username/.ssh/id_rsa"
 done
 
+cat /home/$username/.ssh/id_rsa.pub > /home/$username/.ssh/authorized_keys
+chown $username:$username /home/$username/.ssh/authorized_keys
+
 echo "setup trust"
-for ((i=0; i<HOST_NUM; i++)); do
-    cat /home/$username/.ssh/id_rsa.pub | ssh -o StrictHostKeyChecking=no ${HOST[$i]} "sudo -u $username cat - >> /home/$username/.ssh/authorized_keys"
+for ((i=1; i<HOST_NUM; i++)); do
+    scp /home/$username/.ssh/id_rsa.pub /home/$username/.ssh/id_rsa /home/$username/.ssh/authorized_keys ${HOST[$i]}:/home/$username/.ssh/
+    ssh -o StrictHostKeyChecking=no ${HOST[$i]} "chown $username:$username /home/$username/.ssh/id_rsa.pub /home/$username/.ssh/id_rsa /home/$username/.ssh/authorized_keys"
 done
 
 echo "done"
